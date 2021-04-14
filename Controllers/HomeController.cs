f6e4fcde9b39132f24c63728cce32383967e78d0
erramire <erramire@microsoft.com>
2021-04-14 11:19:06 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PortalDemoLiveEvents.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalDemoLiveEvents.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration _configuration { get; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.OriginalSource = _configuration.GetValue<string>("VideoSources:OriginalSource");
+            ViewBag.SpanishSource = _configuration.GetValue<string>("VideoSources:SpanishSource");
+            ViewBag.EnglishSource = _configuration.GetValue<string>("VideoSources:EnglishSource");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
